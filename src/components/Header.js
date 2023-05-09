2b1dad414a64d48c8111d6b82f7bab1f54c87236
Sharath <sharath.esr@gmail.com>
2023-05-09 20:07:16 +0530

    added header as a component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import {NavLink} from 'react-router-dom';
+
+export function Header(){
+return (
+    <div>
+     <h1>React App</h1>
+     <h1>My Mail Box</h1>
+     <ul>
+      <li><NavLink to="/inbox">Inbox</NavLink></li>
+      <li><NavLink to="/sent">Sent </NavLink></li>
+     </ul>
+
+    </div>
+)
+}
