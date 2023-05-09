694432d925f10bbc9c9d933fa4352017a10fb88b
Sharath <sharath.esr@gmail.com>
2023-05-09 19:52:54 +0530

    intial pagination done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {Routes,Route,NavLink} from 'react-router-dom';
+import {LandingPage,Inbox,Sent} from './pages';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <nav>
+        <NavLink to="/">Landing Page ||</NavLink>
+        <NavLink to="/inbox">Inbox ||</NavLink>
+        <NavLink to="/sent">Sent ||</NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/inbox' element={<Inbox/>}/>
+        <Route path='/sent' element={<Sent/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/inbox.js
@@ -0,0 +1,5 @@
+export function Inbox(){
+    return (
+        <div>Inbox</div>
+    )
+}
