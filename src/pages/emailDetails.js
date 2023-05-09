36439c1176875f20513ce31ee3ffe418f8f92aa3
Sharath <sharath.esr@gmail.com>
2023-05-09 22:14:26 +0530

    added undefined edge case for emailDetails Page

--- src/pages/emailDetails.js
+++ src/pages/emailDetails.js
@@ -6,15 +6,18 @@ export function EmailDetails(){
     const {mailDetailId} = useParams();
     const {emailData: {mails, sentMails}} = useContext(EmailContext);
 
-    const clickedMail = sentMails?.find(mail => mail.id === Number(mailDetailId));
-    const displayMail = mails?.find(mail => mail.subject === clickedMail.subject.slice(4));
+    const clickedMail = sentMails.find(mail => mail.id === Number(mailDetailId));
+    const displayMail = mails.find(mail => mail.subject === clickedMail.subject.slice(4));
    
 
     return (
         <div>
-           <h1>Email Details of {mailDetailId}</h1>
-           <p>from : {displayMail.sender}</p>
+           <h1>Email Details</h1>
+           {displayMail!==undefined && <div> 
+            <p>from : {displayMail.sender}</p>
            <p>Message : {displayMail.message}</p>
+           </div>
+    }
 
         </div>
     )
